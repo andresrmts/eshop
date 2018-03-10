81b759e7e8c1e01f4a1d9eaf6c66c5813ef1a687
andresrmts <andresrmts@gmail.com>
2018-03-10 19:44:54 +0200

    previous commit was slightly faulty

--- src/Product.js
+++ src/Product.js
@@ -38,26 +38,3 @@ export default class Product extends Component {
 	}
 }
 
-
-
-// export const Product = ({img, name, price, currency}) => {
-// 	return (
-// 		<div className = "tc bg-lightest-blue dib br4 pa3 ma2 bw2 shadow-5 w5">
-// 			<img src={img} alt="productphoto" />
-// 			<h1>{name}</h1>
-// 			<p className = "tc bg-green bw1 br-pill fl w-third shadow-3">{price} {currency}</p>
-// 		</div>
-// 	)
-// }
-
-// export const ProductDetails = ({ img, name, price, description, department }) => {
-// 	return (
-// 		<div className = "tc bg-lightest-blue dib br4 pa3 ma2 bw2 shadow-5 w5">
-// 			<img src={img} alt="productphoto" />
-// 			<h1>{name}</h1>
-// 			<p>{description}</p>
-// 			<p>{department}</p>
-// 		</div>
-// 	)
-// }
-
